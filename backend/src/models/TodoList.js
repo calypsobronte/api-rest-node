a1804e4fc8851245ef8e5eb1db56fb3a9c53238b
Lina María Montaño Ramírez <18755556+calypsobronte@users.noreply.github.com>
2020-10-07 00:30:31 -0500

    Update TodoList.js

--- backend/src/models/TodoList.js
+++ backend/src/models/TodoList.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Database creation
 const TodoListSchema = Schema({
     name: {type: String, required: true, max: 30},
     isComplete: { type: Boolean, default: false }
