2d1e1d6860ac9df7904c9b16997d56512bb600f3
Lina María Montaño Ramírez <18755556+calypsobronte@users.noreply.github.com>
2020-10-07 00:32:17 -0500

    Update todoListController.js

--- backend/src/controllers/todoListController.js
+++ backend/src/controllers/todoListController.js
@@ -11,14 +11,12 @@ todoListController.all = async (req, res) => {
 todoListController.create = async (req, res, next) => {
   	const todolist = new TodoList(req.body);
 	await todolist.save();
-  	//res.redirect('/');
   	res.json({ response: "La lista se creo con exito" });
 };
 
 todoListController.selectForId = async (req, res, next) => {
 	let { id } = req.params;
 	await TodoList.findOne({_id: id}).exec((err, todoList) => {
-	//res.redirect('/');
 	res.json(todoList)
 	});
 };
@@ -26,14 +24,12 @@ todoListController.selectForId = async (req, res, next) => {
 todoListController.update = async (req, res, next) => {
   	const { id } = req.params;
   	await TodoList.findOneAndUpdate({_id: id}, { $set: { isComplete: req.body.isComplete} }, { new: true });
-	//res.redirect('/');
 	res.json({ response: "La lista se actualizo con exito" });
 };
 
 todoListController.delete = async (req, res, next) => {
 	let { id } = req.params;
 	await TodoList.remove({_id: id});
-	//res.redirect('/');
 	res.json({ response: "La lista se elimino con exito" });
 };
 
